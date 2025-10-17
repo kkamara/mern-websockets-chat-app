87ad60c66f2f9d3c9e64f3eb3f1603c78f28d511
kkamara <kamaracomputers@gmail.com>
2025-10-17 17:14:16 +0100

    Creating APIs with ExpressJS 2

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,14 @@ app.get("/api/chat", (req, res) => {
   res.send(chats);
 });
 
-app.get("/api/chat/")
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find(
+    c => c._id === req.params.id
+  );
+  res.send(singleChat);
+});
 
-app.listen(5000, console.log("Server started on PORT 5000."));
+app.listen(
+  5000,
+  console.log("Server started on PORT 5000."),
+);
