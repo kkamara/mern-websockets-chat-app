8999c087e621b30776a547fb43741c0c181be379
kkamara <kamaracomputers@gmail.com>
2025-10-27 16:16:04 +0000

    Login and Signup Forms UI in React JS

---
+++ frontend/src/components/Authentication/Signup.js
@@ -0,0 +1,75 @@
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react';
+import React, { useState, } from 'react';
+
+const Signup = () => {
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmpassword, setConfirmpassword] = useState("");
+  const [password, setPassword] = useState("");
+  const [pic, setPic] = useState(null);
+
+  const handleClick = () => setShow(!show);
+
+  const postDetails = pics => {};
+
+  const submitHandler = () => {};
+
+  return (
+    <VStack spacing="5px" color="black">
+      <FormControl id="first-name" isRequired>
+        <FormLabel>Name</FormLabel>
+        <Input
+          placeholder='Enter Your Name'
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder='Enter Your Email'
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder='Enter Your Password'
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="pic">
+        <FormLabel>Upload Your Picture</FormLabel>
+        <Input
+          type={"file"}
+          p={1.5}
+          accept="image/*"
+          onChange={e => postDetails(e.target.files[0])}
+        />
+      </FormControl>
+
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+        // isLoading={picLoading}
+      >
+        Sign Up
+      </Button>
+    </VStack>
+  )
+};
+
+export default Signup;
