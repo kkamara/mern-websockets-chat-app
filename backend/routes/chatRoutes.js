0c16e2469df087af7044494b244ae63ec38616a3
kkamara <kamaracomputers@gmail.com>
2025-10-30 16:58:29 +0000

    Add User to Group API

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -5,6 +5,7 @@ const {
   fetchChats,
   createGroupChat,
   renameGroup,
+  addToGroup,
 } = require("../controllers/chatControllers");
 
 const router = express.Router();
@@ -14,6 +15,6 @@ router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
 router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
-// router.route("/groupadd").put(protect, addToGroup);
+router.route("/groupadd").put(protect, addToGroup);
 
 module.exports = router;
