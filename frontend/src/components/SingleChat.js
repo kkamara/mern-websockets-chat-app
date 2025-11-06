d770203502ad90ad9024df429a5da80fa8fd3948
kkamara <kamaracomputers@gmail.com>
2025-11-06 15:05:53 +0000

    Join a Chat

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -96,6 +96,8 @@ const SingleChat = ({ fetchAgain, setFetchAgain, }) => {
       
       setMessages(data);
       setLoading(false);
+
+      socket.emit("join chat", selectedChat._id);
     } catch (error) {
       toast({
         title: "Error Occurred!",
